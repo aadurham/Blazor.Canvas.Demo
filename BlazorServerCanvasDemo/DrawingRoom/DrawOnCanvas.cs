655877470b992c7d46a49991123fdd7c4d0fee05
aadurham <atila.abdulkadiroglu@gmail.com>
2021-02-25 16:08:01 -0500

    initial committ

---
+++ BlazorServerCanvasDemo/DrawingRoom/DrawOnCanvas.cs
@@ -0,0 +1,44 @@
+﻿using Excubo.Blazor.Canvas;
+using Excubo.Blazor.Canvas.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCanvasDemo.DrawingRoom
+{
+    public static class DrawOnCanvas
+    {
+       
+        public static async void Line(Context2D context, Line line)
+        {
+
+            await context.BeginPathAsync();
+            await context.MoveToAsync(line.startX, line.startY);
+            await context.LineToAsync(line.endX, line.endY);
+            await context.LineWidthAsync(line.thickness);
+
+            // set line color
+            await context.StrokeStyleAsync(line.color);
+            await context.StrokeAsync();
+        }
+
+        public static async void Line(Canvas helper_canvas, Line line)
+        {
+            await using (var context = await helper_canvas.GetContext2DAsync())
+            {
+
+                await context.BeginPathAsync();
+                await context.MoveToAsync(line.startX, line.startY);
+                await context.LineToAsync(line.endX, line.endY);
+                await context.LineWidthAsync(line.thickness);
+
+                // set line color
+                await context.StrokeStyleAsync(line.color);
+                await context.StrokeAsync();
+            }
+        }
+
+
+    }
+}
---
+++ BlazorServerCanvasDemo/DrawingRoom/Line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCanvasDemo.DrawingRoom
+{
+    public class Line
+    {
+        public int startX { get; set; }
+        public int startY { get; set; }
+        public int endX { get; set; }
+        public int endY { get; set; }
+        public int thickness { get; set; }
+        public string color { get; set; } = "black";
+
+        public Line(int startx, int starty, int endx, int endy, int lineThickness, string lineColor)
+        {
+            startX = startx;
+            startY = starty;
+            endX = endx;
+            endY = endy;
+            thickness = lineThickness;
+            color = lineColor;
+        }
+    }
+}
